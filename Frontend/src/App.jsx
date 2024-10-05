4d619142f284be7991782d413d9c503f0ee5ea12
truonghiep <truonghiep051103@gmail.com>
2024-10-05 22:16:27 +0700

    Login & User

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,26 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import MessagePage from "./pages/Message";
-import DefaultLayout from "./components/Layouts/DefaultLayout";
-import Profile from "./pages/Profile";
-import ProfileGroup from "./pages/ProfileGroup";
+// import MessagePage from "./pages/Message";
+// import DefaultLayout from "./components/Layouts/DefaultLayout";
+// import Profile from "./pages/Profile";
+// import ProfileGroup from "./pages/ProfileGroup";
+import Login from "./pages/Login/index.jsx";
+import Information from "./pages/Information/index.jsx";
 
 function App() {
     return (
         <Router>
-        <DefaultLayout>
+        {/* <DefaultLayout>
                 <Routes>
                     <Route path="/message" element={<MessagePage />} />
                     <Route path="/" element={<MessagePage />} />
                     <Route path="/group" element={<MessagePage />} />
                     <Route path="/profile" element={<Profile />} />
                 </Routes>
-        </DefaultLayout>
+        </DefaultLayout> */}
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/info" element={<Information />} />
+        </Routes>
             </Router>
     );
 }
