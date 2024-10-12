fad3d7a6991ecb25e4432ea76b9e20868997dfde
Toan-LD <101153912+Toan-LD@users.noreply.github.com>
2024-10-12 09:36:47 +0700

    sua loi

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,52 +1,11 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
-import { useState,createContext } from "react";
-
-import MessagePage from "./pages/Message";
-import DefaultLayout from "./components/Layouts/DefaultLayout";
-import Profile from "./pages/Profile";
-// import ProfileGroup from "./pages/ProfileGroup";
-import Login from "./pages/Login/index.jsx";
-import Information from "./pages/Information/index.jsx";
-import ProfileGroup from "./pages/ProfileGroup/index.jsx";
-
-
-function App() {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    
-    return (
-        <Router>
-            {isAuthenticated ? (
-                <DefaultLayout>
-                    <Routes>
-                        <Route path="/message" element={<MessagePage />} />
-                        <Route path="/" element={<MessagePage />} />
-                        <Route path="/group" element={<MessagePage />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="/information" element={<Information />} />
-                    </Routes>
-                </DefaultLayout>
-            ) : (
-                <Routes>
-                    <Route
-                        path="/login"
-                        element={
-                            <Login onLogin={() => {
-                                setIsAuthenticated(true)
-                                
-                                }
-                                } />
-                        }
-                    />
-                    <Route path="*" element={<Navigate to="/login" />} />
-                </Routes>
-            )}
-        </Router>
-    );
-}
-
-export default App;
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyles from "./components/GlobalStyles/index.jsx";
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </StrictMode>
+);
