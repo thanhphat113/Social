a2977a9b4b970ab1b94a96c5770b254443aaa9bf
thanhphat113 <thanhphat9523@gmail.com>
2024-11-21 22:54:33 +0700

    fix

--- Frontend/src/components/Redux/Actions/MessageActions.jsx
+++ Frontend/src/components/Redux/Actions/MessageActions.jsx
@@ -1,6 +1,22 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const getMess = createAsyncThunk(
+    "message/main",
+    async (AnotherUser, thunkAPI) => {
+        try {
+            const response = await axios.get(
+                `http://localhost:5164/api/Message`,
+                { params: { id: AnotherUser }, withCredentials: true }
+            );
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(
+                error.response ? error.response.data : error.message
+            );
+        }
+    }
+);
 
 const addMess = createAsyncThunk(
     "message/chat",
@@ -11,44 +27,70 @@ const addMess = createAsyncThunk(
                 { MessagesId, FromUser, Content, Otheruser },
                 { withCredentials: true }
             );
-            return {friendId: Otheruser, message: response.data};
+            return { friendId: Otheruser, message: response.data };
         } catch (error) {
-            return thunkAPI.rejectWithValue(error.response ? error.response.data : error.message);
+            return thunkAPI.rejectWithValue(
+                error.response ? error.response.data : error.message
+            );
+        }
+    }
+);
+
+const recallMess = createAsyncThunk(
+    "message/recall",
+    async ({ id, Otheruser }, thunkAPI) => {
+        console.log("đây là: " + id);
+        try {
+            const response = await axios.post(
+                `http://localhost:5164/api/ChatInMessage/recall`,
+                id,
+                {
+                    headers: { "Content-Type": "application/json" },
+                    withCredentials: true,
+                }
+            );
+
+            return { isRecall: response.data, friendId: Otheruser, chatId: id };
+        } catch (error) {
+            console.log("lôiix");
+            return thunkAPI.rejectWithValue(
+                error.response ? error.response.data : error.message
+            );
         }
     }
 );
 
 const deleteMess = createAsyncThunk(
     "message/delete",
-    async ({ id, Otheruser } , thunkAPI) => {
+    async ({ id, Otheruser }, thunkAPI) => {
         try {
             const response = await axios.delete(
                 `http://localhost:5164/api/ChatInMessage/${id}`,
                 { withCredentials: true }
             );
 
-            return { isDelete: response.data, friendId: Otheruser, chatId: id};
+            return { isDelete: response.data, friendId: Otheruser, chatId: id };
         } catch (error) {
-            return thunkAPI.rejectWithValue(error.response ? error.response.data : error.message);
-        }
-    }
-);
-
-const readMess = createAsyncThunk(
-    "message/read",
-    async ( id , thunkAPI) => {
-        console.log(id)
-        try {
-            const response = await axios.put(
-                `http://localhost:5164/api/ChatInMessage/${id}`,
-                { },
-                { withCredentials: true }
+            return thunkAPI.rejectWithValue(
+                error.response ? error.response.data : error.message
             );
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.response ? error.response.data : error.message);
         }
     }
 );
 
-export { addMess, readMess, deleteMess };
+const readMess = createAsyncThunk("message/read", async (id, thunkAPI) => {
+    try {
+        const response = await axios.put(
+            `http://localhost:5164/api/ChatInMessage/${id}`,
+            {},
+            { withCredentials: true }
+        );
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(
+            error.response ? error.response.data : error.message
+        );
+    }
+});
+
+export { getMess, addMess, readMess, recallMess, deleteMess };
